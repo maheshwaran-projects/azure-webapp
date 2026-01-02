3614ec928429b58731df5e573c282b7c7e48b2e7
mahesh-assessment <maheshassessment@gmail.com>
2026-01-02 16:28:20 +0530

    Update keyvault.tf

--- Infra/terraform/keyvault.tf
+++ Infra/terraform/keyvault.tf
@@ -1,16 +1,17 @@
 # keyvault.tf
 data "azurerm_client_config" "current" {}
 
+# Use the existing Key Vault you created
 resource "azurerm_key_vault" "kv" {
-  name                       = "kv-quote-${random_id.rand.hex}"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
+  name                = "kv-quote-155982b"  # Your existing Key Vault
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
-  # Grant yourself full access
+  # Grant yourself access (needed for Terraform to read)
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
@@ -37,40 +38,22 @@ resource "azurerm_key_vault" "kv" {
       "List"
     ]
   }
-
-  tags = {
-    environment = "production"
-  }
 }
 
-# Data sources to read existing secrets (will be created by CLI script)
+# Read the Base64 certificate from Key Vault
+data "azurerm_key_vault_secret" "appgw_certificate_base64" {
+  name         = "appgw-certificate-base64"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+# Read the certificate password
 data "azurerm_key_vault_secret" "appgw_cert_password" {
   name         = "appgw-cert-password"
   key_vault_id = azurerm_key_vault.kv.id
-  
-  # This will fail on first run, but that's OK - we'll create secrets after KV
-  depends_on = [azurerm_key_vault.kv]
 }
 
+# Read SQL admin password
 data "azurerm_key_vault_secret" "sql_admin_password" {
   name         = "sql-admin-password"
   key_vault_id = azurerm_key_vault.kv.id
-  
-  depends_on = [azurerm_key_vault.kv]
-}
-
-# Store certificate in Key Vault too (optional but recommended)
-resource "azurerm_key_vault_certificate" "appgw" {
-  name         = "appgw-ssl-certificate"
-  key_vault_id = azurerm_key_vault.kv.id
-
-  certificate {
-    contents = filebase64("${path.module}/certificates/cloudapp.pfx")
-    password = data.azurerm_key_vault_secret.appgw_cert_password.value
-  }
-
-  depends_on = [
-    azurerm_key_vault.kv,
-    data.azurerm_key_vault_secret.appgw_cert_password
-  ]
 }
