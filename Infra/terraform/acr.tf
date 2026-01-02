c1a89d9314a7f8f7b62b15a6eb4af5e5606699da
mahesh-assessment <maheshassessment@gmail.com>
2026-01-02 22:38:15 +0530

    Update acr.tf

--- Infra/terraform/acr.tf
+++ Infra/terraform/acr.tf
@@ -1,4 +1,4 @@
-#####
+###
 resource "azurerm_container_registry" "acr" {
   name                = "quoteacr${random_id.rand.hex}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -10,5 +10,6 @@ resource "azurerm_container_registry" "acr" {
 resource "random_id" "rand" {
   byte_length = 4
 }
+
 
 
