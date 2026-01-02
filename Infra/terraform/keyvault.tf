0e22b3765d12dc62f4abc0a831b26fcfa513836c
mahesh-assessment <maheshassessment@gmail.com>
2026-01-02 21:56:01 +0530

    Update keyvault.tf

--- Infra/terraform/keyvault.tf
+++ Infra/terraform/keyvault.tf
@@ -1,9 +1,33 @@
 # keyvault.tf
 data "azurerm_client_config" "current" {}
 
-# Use the existing Key Vault you created
-resource "azurerm_key_vault" "kv" {
-  name                = "kv-quote-155982b"  # Your existing Key Vault
+# Reference the EXISTING Key Vault in rg-tfstate-prod
+data "azurerm_key_vault" "secrets" {
+  name                = "kv-quote-app"            # Your Key Vault name
+  resource_group_name = "rg-tfstate-prod"         # Different resource group
+}
+
+# Read secrets from the existing Key Vault
+data "azurerm_key_vault_secret" "appgw_certificate_base64" {
+  name         = "appgw-certificate-base64"
+  key_vault_id = data.azurerm_key_vault.secrets.id
+}
+
+data "azurerm_key_vault_secret" "appgw_cert_password" {
+  name         = "appgw-cert-password"
+  key_vault_id = data.azurerm_key_vault.secrets.id
+}
+
+data "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = "sql-admin-password"
+  key_vault_id = data.azurerm_key_vault.secrets.id
+}
+
+# Optional: Grant AKS access through a local Key Vault copy
+resource "azurerm_key_vault" "local_for_aks" {
+  count = var.create_local_key_vault ? 1 : 0
+
+  name                = "kv-quote-aks-${random_id.rand.hex}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -11,24 +35,7 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
-  # Grant yourself access (needed for Terraform to read)
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
-      "Purge"
-    ]
-  }
-
-  # Grant AKS identity access to read secrets
+  # Grant AKS identity access
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = azurerm_kubernetes_cluster.aks.identity[0].principal_id
@@ -38,22 +45,28 @@ resource "azurerm_key_vault" "kv" {
       "List"
     ]
   }
+
+  # Grant yourself access for management
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete"
+    ]
+  }
 }
 
-# Read the Base64 certificate from Key Vault
-data "azurerm_key_vault_secret" "appgw_certificate_base64" {
-  name         = "appgw-certificate-base64"
-  key_vault_id = azurerm_key_vault.kv.id
-}
+# Copy SQL password to local KV for AKS access
+resource "azurerm_key_vault_secret" "local_sql_password" {
+  count = var.create_local_key_vault ? 1 : 0
 
-# Read the certificate password
-data "azurerm_key_vault_secret" "appgw_cert_password" {
-  name         = "appgw-cert-password"
-  key_vault_id = azurerm_key_vault.kv.id
-}
-
-# Read SQL admin password
-data "azurerm_key_vault_secret" "sql_admin_password" {
   name         = "sql-admin-password"
-  key_vault_id = azurerm_key_vault.kv.id
+  value        = data.azurerm_key_vault_secret.sql_admin_password.value
+  key_vault_id = azurerm_key_vault.local_for_aks[0].id
+  
+  depends_on = [azurerm_key_vault.local_for_aks]
 }
