f8e489650fbc4a2f09eaff319f26c618a710004b
mahesh-assessment <maheshassessment@gmail.com>
2026-01-02 11:46:49 +0530

    Update providers.tf

--- Infra/terraform/providers.tf
+++ Infra/terraform/providers.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "74c4f319-b9f6-4b4f-b910-b6bb2923cf97"
+  use_azuread_auth     = true
+  subscription_id      = "74c4f319-b9f6-4b4f-b910-b6bb2923cf97"
+  tenant_id           = "105b2061-b669-4b31-92ac-24d304d195dc"
+  client_id           = "c17b1a62-a546-4391-a7b8-2f043d6cddae"
 }
+
