a0ffab01ce8120eabda4aa6a199d84afe9b6b018
mahesh-assessment <maheshassessment@gmail.com>
2026-01-02 11:30:53 +0530

    Update backend.tf

--- Infra/terraform/backend.tf
+++ Infra/terraform/backend.tf
@@ -4,5 +4,8 @@ terraform {
     storage_account_name = "tfstatequote525"
     container_name       = "tfstate"
     key                  = "quote-app/production.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "74c4f319-b9f6-4b4f-b910-b6bb2923cf97"
+    tenant_id           = var.tenant_id
   }
 }
