e2db1ade4de3294c445286745f5a296100b195ec
mahesh-assessment <maheshassessment@gmail.com>
2026-01-03 10:23:05 +0530

    Update backend.tf

--- Infra/terraform/backend.tf
+++ Infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate-prod"
+    resource_group_name  = "rg-tfstate-vault"
     storage_account_name = "tfstatequote525"
     container_name       = "tfstate"
     key                  = "quote-app/production.tfstate"
