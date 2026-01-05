bc82bd1ec5fe2aa3a949de7c52b1e599dee7b588
root <root@HPE-5CG2184QNH.localdomain>
2026-01-06 00:26:24 +0530

    fix in code restructure

--- Infra/terraform/environments/poc/main.tf
+++ Infra/terraform/environments/poc/main.tf
@@ -12,12 +12,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate-vault"
-    storage_account_name = "tfstatequote525"
-    container_name       = "tfstate"
-    key                  = "quote-app/poc.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
